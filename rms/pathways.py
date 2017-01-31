0190068fcd03e9d2c9980aaa5f9220f3652c7b07
fredkingham <fredkingham@gmail.com>
2017-01-31 16:48:51 +0000

    demographics save

--- rms/pathways.py
+++ rms/pathways.py
@@ -6,6 +6,7 @@ import datetime
 
 from pathway.pathways import PagePathway, Step
 from rms import models
+from opal import models as omodels
 
 
 class ReferralPathway(PagePathway):
@@ -26,6 +27,7 @@ class ReferralPathway(PagePathway):
     )
 
     def save(self, data, user):
+        data[models.Demographics.get_api_name()][0]["hospital_number"] = omodels.Patient.objects.count()
         patient = super(ReferralPathway, self).save(data, user)
         episode = patient.episode_set.last()
         episode.active = True
