83c87a860258495e909b0a01c03a475802a3dce5
David Miller <david@deadpansincerity.com>
2017-01-31 09:42:46 +0000

    Make it so

---
+++ README.md
@@ -0,0 +1,8 @@
+This is rms - an [OPAL](https://github.com/openhealthcare/opal) project.
+
+To get started, run the following commands:
+
+```
+    python manage.py migrate
+    python manage.py runserver
+```
---
+++ rms/__init__.py
@@ -0,0 +1,13 @@
+"""
+rms - Our OPAL Application
+"""
+from opal.core import application
+
+class Application(application.OpalApplication):
+    flow_module   = 'rms.flow'
+    javascripts   = [
+        'js/rms/routes.js',
+        'js/opal/controllers/discharge.js',
+        # Uncomment this if you want to implement custom dynamic flows.
+        # 'js/rms/flow.js',
+    ]
