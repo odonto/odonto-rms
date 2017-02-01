2814e7782211c8c6c06452c784a649dedd34faec
fredkingham <fredkingham@gmail.com>
2017-02-01 11:03:06 +0000

    adding in confirmation

---
+++ rms/admin.py
@@ -0,0 +1,4 @@
+from django.contrib import admin
+from rms import models
+
+admin.site.register(models.ClinicLocation)
--- rms/static/js/rms/controllers/allocated_clinic_location_helper.js
+++ rms/static/js/rms/controllers/allocated_clinic_location_helper.js
@@ -1,35 +1,50 @@
 angular.module('opal.controllers').controller(
-    'AllocatedClinicLocationHelper',
-    function(
-      $scope, $window, recordLoader, ngProgressLite, $q,
-      $route, Metadata
-    ){
-      "use strict";
-      var self = this;
-      var allocated_clinic;
-      self.saving = false;
-      self.editing = {allocated_clinic: {}};
+  'AllocatedClinicLocationHelper',
+  function(
+    $scope, $window, recordLoader, ngProgressLite, $q,
+    $route){
+    "use strict";
+    var self = this;
+    var allocated_clinic;
+    self.saving = false;
+    self.editing = {allocated_clinic: {}};
+    self.confirmed = false;
 
-      Metadata.then(function(metadata){
-        self.metadata = metadata;
-      });
+    recordLoader.then(function(){
+      if($scope.episode.allocated_clinic && $scope.episode.allocated_clinic.length){
+        allocated_clinic = $scope.episode.allocated_clinic[0];
+      }
+      else{
+        allocated_clinic = $scope.episode.newItem("allocated_clinic");
+      }
+      var changed = allocated_clinic.makeCopy();
 
-      recordLoader.then(function(){
-        if($scope.episode.allocated_clinic && $scope.episode.allocated_clinic.length){
-          allocated_clinic = $scope.episode.allocated_clinic[0];
-        }
-        else{
-          allocated_clinic = $scope.episode.newItem("allocated_clinic");
-        }
-        var changed = allocated_clinic.makeCopy();
-        self.editing.allocated_clinic = changed;
+      self.confirmed = allocated_clinic.confirmed;
 
-        self.submit = function(){
+      if(!self.confirmed){
+        if(allocated_clinic.location_id){
+          self.confirm = function(){
+            changed.confirmed = true;
             self.saving = true;
             allocated_clinic.save(changed).then(function(){
-                self.saving = false;
-                $route.reload();
+              self.saving = false;
+              $route.reload();
+            });
+          }
+        }
+        else{
+          self.editing.allocated_clinic = changed;
+
+          self.submit = function(){
+            self.saving = true;
+            allocated_clinic.save(changed).then(function(){
+              self.saving = false;
+              $route.reload();
             });
           };
-        });
-      });
+        }
+      }
+
+
+    });
+  });
