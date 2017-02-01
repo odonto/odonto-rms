94e1e22514e5468663cec17589eeab1243a9fd21
fredkingham <fredkingham@gmail.com>
2017-02-01 14:30:27 +0000

    let the img view do the appropriate lookups

--- rms/urls.py
+++ rms/urls.py
@@ -9,7 +9,7 @@ from rms import views
 urlpatterns = patterns(
     '',
     url(r'^admin/', include(admin.site.urls)),
-    url(r'^imgs/?$', views.ImgTemplateView.as_view()),
+    url(r'^imgs/(?P<model>[a-z_\-]+)/(?P<pk>[0-9]+/?)/?$', views.ImgTemplateView.as_view()),
 )
 
 urlpatterns += opatterns
