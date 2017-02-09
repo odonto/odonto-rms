f6fbc3639391110779bfe5dcc147beb9e1d48b74
fredkingham <fredkingham@gmail.com>
2017-02-09 14:27:55 +0000

    add in switch user overlay

--- rms/urls.py
+++ rms/urls.py
@@ -9,7 +9,14 @@ from rms import views
 urlpatterns = patterns(
     '',
     url(r'^admin/', include(admin.site.urls)),
-    url(r'^imgs/(?P<model>[a-z_\-]+)/(?P<pk>[0-9]+/?)/?$', views.ImgTemplateView.as_view()),
+    url(
+        r'^imgs/(?P<model>[a-z_\-]+)/(?P<pk>[0-9]+/?)/?$',
+        views.ImgTemplateView.as_view()
+    ),
+    url(
+        r'^change_user/(?P<user>[a-z_\-]+)/?$',
+        views.ChangeUser.as_view(), name="change_user"
+    ),
 )
 
 urlpatterns += opatterns
--- rms/views.py
+++ rms/views.py
@@ -1,4 +1,6 @@
-from django.views.generic import TemplateView
+from django.views.generic import TemplateView, RedirectView
+from django.contrib.auth.models import User
+from django.contrib.auth import logout, login, authenticate
 from opal.core.views import LoginRequiredMixin
 from opal.core.subrecords import get_subrecord_from_api_name
 
@@ -12,3 +14,24 @@ class ImgTemplateView(LoginRequiredMixin, TemplateView):
         context = super(ImgTemplateView, self).get_context_data(**kwargs)
         context["title"] = instance.name
         return context
+
+
+class ChangeUser(RedirectView):
+    def dispatch(self, *args, **kwargs):
+        self.user = User.objects.get(username=kwargs.pop("user"))
+        logout(self.request)
+        authenticated = authenticate(
+            username=self.user.username,
+            password="{}1".format(self.user.username)
+        )
+        login(self.request, authenticated)
+        return super(ChangeUser, self).dispatch(*args, **kwargs)
+
+    def get_redirect_url(self, *args, **kwargs):
+        if self.user.username == "linda":
+            return "/#/list/my_referrals"
+        if self.user.username == "mary":
+            return "/#/list/new_referrals"
+        elif self.user.username == "matt":
+            return "/#/list/approval_inbox"
+        return self.request.get_full_path()
