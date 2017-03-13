fad3cd95cc9503196e9433d6c57a504ee7717363
David Miller <david@deadpansincerity.com>
2017-03-13 16:45:27 +0000

    Update refferal details admin

--- rms/admin.py
+++ rms/admin.py
@@ -1,4 +1,12 @@
 from django.contrib import admin
 from rms import models
 
+from opal.admin import EpisodeSubrecordAdmin
+
+admin.site.unregister(models.ReferralDetails)
+
+class ReferralDetailsAdmin(EpisodeSubrecordAdmin):
+    list_display = ['__unicode__', 'when', 'who']
+
+admin.site.register(models.ReferralDetails, ReferralDetailsAdmin)
 admin.site.register(models.ClinicLocation)
